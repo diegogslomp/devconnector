90f1b9df397c08cffb4b8875f0cab98dd9e92a88
Diego Gobbi Slomp <diegogslomp@gmail.com>
2019-02-28 10:14:50 -0300

    testing registration with form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Register extends Component {
   constructor() {
@@ -24,7 +25,10 @@ class Register extends Component {
       password2: this.state.password2
     };
     e.preventDefault();
-    console.log(newUser);
+    axios
+      .post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data));
   };
 
   render() {
