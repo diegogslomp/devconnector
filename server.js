6742a4e98e00f78de8f960e23bed345bb90141a7
Diego Gobbi Slomp <diegogslomp@gmail.com>
2019-05-30 09:30:01 -0300

    add test r outes

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+connectDB();
+
 app.get('/', (req, res) => res.send('API running'));
 
-const PORT = process.env.PORT || 8000;
+// Define Routes
+app.use('/api/users', require('./user/api/routes'));
+app.use('/api/auth', require('./auth/api/routes'));
+app.use('/api/profile', require('./profile/api/routes'));
+app.use('/api/posts', require('./post/api/routes'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
