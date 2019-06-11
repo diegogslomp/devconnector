6109f95eccdb30c74bf45e6f786e16598d1868ac
Diego Gobbi Slomp <diegogslomp@gmail.com>
2019-06-11 16:20:32 -0300

    user login

--- api/auth/router.js
+++ api/auth/router.js
@@ -2,6 +2,10 @@ const express = require('express');
 const auth = require('./middleware');
 const router = express.Router();
 const User = require('../user/model');
+const { check, validationResult } = require('express-validator/check');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // @route  GET api/auth
 // @desc   Test route
@@ -15,5 +19,61 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route  POST api/auth
+// @desc   Auhtenticate User & Get Token
+// @access Public
+router.post(
+  '/',
+
+  [
+    check('email', 'Invalid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+
+  async (req, res) => {
+    try {
+      // Get errors if validation result failed
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      // Get info from post request
+      const { email, password } = req.body;
+
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      payload = { id: user.id };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,11 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -26,6 +26,10 @@ const Register = ({ setAlert, register }) => {
     }
   };
 
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
   return (
     <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
@@ -40,7 +44,7 @@ const Register = ({ setAlert, register }) => {
             placeholder="Name"
             value={name}
             onChange={e => onChange(e)}
-            // required
+            required
           />
         </div>
         <div className="form-group">
@@ -50,7 +54,7 @@ const Register = ({ setAlert, register }) => {
             name="email"
             value={email}
             onChange={e => onChange(e)}
-            // required
+            required
           />
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
@@ -65,7 +69,7 @@ const Register = ({ setAlert, register }) => {
             minLength="6"
             value={password}
             onChange={e => onChange(e)}
-            // required
+            required
           />
         </div>
         <div className="form-group">
@@ -76,7 +80,7 @@ const Register = ({ setAlert, register }) => {
             minLength="6"
             value={password2}
             onChange={e => onChange(e)}
-            // required
+            required
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
@@ -90,10 +94,15 @@ const Register = ({ setAlert, register }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
-  register: PropTypes.func.isRequired
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   { setAlert, register }
 )(Register);
